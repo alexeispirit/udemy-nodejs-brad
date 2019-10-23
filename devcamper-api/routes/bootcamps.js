a0f1ae8fc7cca364d53ee57f0b7526985779d4c0
alexeispirit <alexeispirit@gmail.com>
2019-10-23 09:09:57 +0500

    61-Review-model-and-get-reviews

--- devcamper-api/routes/bootcamps.js
+++ devcamper-api/routes/bootcamps.js
@@ -15,6 +15,7 @@ const Bootcamp = require("../models/Bootcamp");
 
 // Include other resourse routers
 const courseRouter = require("./courses");
+const reviewRouter = require("./reviews");
 
 // Protect middleware
 const advancedResults = require("../middleware/advancedResults");
@@ -22,6 +23,7 @@ const { protect, authorize } = require("../middleware/auth");
 
 // Re-route into other resourse routers
 router.use("/:bootcampId/courses", courseRouter);
+router.use("/:bootcampId/reviews", reviewRouter);
 
 router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
